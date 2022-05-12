4a0b6227f31a1b92be30967ddf91541a48dd620a
Immo Huneke <immo.huneke@zuhlke.com>
2022-05-12 10:48:12 +0100

    (chore): Tidy up imports; get rid of warnings

--- RestDemo/src/main/java/com/zuhlke/training/restdemo/dao/UserDao.java
+++ RestDemo/src/main/java/com/zuhlke/training/restdemo/dao/UserDao.java
@@ -2,9 +2,6 @@ package com.zuhlke.training.restdemo.dao;
 
 import com.zuhlke.training.restdemo.entity.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.persistence.Query;
 import java.util.List;
 
@@ -14,6 +11,7 @@ public class UserDao extends PersistenceManager{
             String sql = "SELECT c FROM User c where c.id=:id";
             Query query = getEntityManager().createQuery(sql, User.class);
             query.setParameter("id", id);
+            @SuppressWarnings("unchecked")
             List<User> users = query.getResultList();
             for (User user : users) {
                 System.out.println(user);
@@ -31,6 +29,7 @@ public class UserDao extends PersistenceManager{
         try {
             String sql = "SELECT c FROM User c ";
             Query query = getEntityManager().createQuery(sql, User.class);
+            @SuppressWarnings("unchecked")
             List<User> users = query.getResultList();
             for (User user : users) {
                 System.out.println(user);
--- RestDemo/src/main/java/com/zuhlke/training/restdemo/service/UserService.java
+++ RestDemo/src/main/java/com/zuhlke/training/restdemo/service/UserService.java
@@ -3,11 +3,7 @@ package com.zuhlke.training.restdemo.service;
 import com.zuhlke.training.restdemo.dao.UserDao;
 import com.zuhlke.training.restdemo.entity.User;
 
-import javax.enterprise.inject.Any;
-import javax.enterprise.inject.Default;
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class UserService {
@@ -20,6 +16,5 @@ public class UserService {
 
     public List<User> getUsers() {
         return userDao.getUsers();
-        //return Arrays.asList();
     }
 }
