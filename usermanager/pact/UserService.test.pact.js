abf59f98fd9fb76475b8e540f0b7bb13edd0b667
Immo Huneke <immo.huneke@zuhlke.com>
2022-05-13 11:08:00 +0100

    (feat): add second API test

--- usermanager/pact/UserService.test.pact.js
+++ usermanager/pact/UserService.test.pact.js
@@ -1,54 +1,72 @@
 import * as Pact from '@pact-foundation/pact';
 import UserService from "../src/UserService";
+
 const hostUrl = require('./jest.config.js').testURL;
 
 describe('UserService API', () => {
-    const userService = new UserService(hostUrl+"/restdemo");
+    const userService = new UserService(hostUrl + "/restdemo");
+    const contentTypeJsonMatcher = Pact.Matchers.term({
+        matcher: "application\\/json; *charset=utf-8",
+        generate: "application/json; charset=utf-8"
+    });
 
-    describe('getUsers()', () => {
-
-        beforeEach((done) => {
-            const contentTypeJsonMatcher = Pact.Matchers.term({
-                matcher: "application\\/json; *charset=utf-8",
-                generate: "application/json; charset=utf-8"
-            });
-
-            global.provider.addInteraction({
-                state: 'provider allows list of users',
-                uponReceiving: 'a GET request to list users',
-                withRequest: {
-                    method: 'GET',
-                    path: '/restdemo/api/user',
-                    headers: {
-                        Accept: 'application/json, text/plain, */*'
-                    }
-                },
-                willRespondWith: {
-                    status: 202,
-                    headers: {
-                        'Content-Type': contentTypeJsonMatcher
-                    },
-                    body: Pact.Matchers.somethingLike(
-                        [{"email":"gpotawad@gmail.com","id":1,"name":"Gayatri"},{"id":2,"name":"Immo"}]
-                    )
+    it('requests all users', (done) => {
+        global.provider.addInteraction({
+            state: 'database contains two users',
+            uponReceiving: 'a GET request to list users',
+            withRequest: {
+                method: 'GET',
+                path: '/restdemo/api/user',
+                headers: {
+                    Accept: 'application/json, text/plain, */*'
                 }
-            }).then(() => done());
-
-        });
-
-        it('sends a request according to contract', (done) => {
+            },
+            willRespondWith: {
+                status: 202,
+                headers: {
+                    'Content-Type': contentTypeJsonMatcher
+                },
+                body: Pact.Matchers.somethingLike(
+                    [{"email": "gpotawad@gmail.com", "id": 1, "name": "Gayatri"}, {"id": 2, "name": "Immo"}]
+                )
+            }
+        }).then(() => {
             userService.getUsers()
                 .then(response => {
                     expect(response.length).toBeGreaterThanOrEqual(2);
                 })
-                .then(() => {
-                    global.provider.verify()
-                        .then(() => done(), error => {
-                            done.fail(error)
-                        })
-                });
+                .then(done)
+                .catch(done)
         });
-
     });
 
+    it('requests an existing user', (done) => {
+        global.provider.addInteraction({
+            state: 'database contains two users',
+            uponReceiving: 'a GET request to obtain user details',
+            withRequest: {
+                method: 'GET',
+                path: '/restdemo/api/user/1',
+                headers: {
+                    Accept: 'application/json, text/plain, */*'
+                }
+            },
+            willRespondWith: {
+                status: 202,
+                headers: {
+                    'Content-Type': contentTypeJsonMatcher
+                },
+                body: Pact.Matchers.somethingLike(
+                    {"email": "gpotawad@gmail.com", "id": 1, "name": "Gayatri"}
+                )
+            }
+        }).then(() => {
+            userService.getUserDetails("1")
+                .then(response => {
+                    expect(response.name).toEqual("Gayatri");
+                })
+                .then(done)
+                .catch(done);
+        });
+    });
 });
