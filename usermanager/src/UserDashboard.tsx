4ac7bdafc98f2a637c03076818d20abfb6ca973b
Immo Huneke <immo.huneke@zuhlke.com>
2022-05-16 14:44:21 +0100

    (fix): Add thead/tbody elements to the dashboard

--- usermanager/src/UserDashboard.tsx
+++ usermanager/src/UserDashboard.tsx
@@ -4,9 +4,9 @@ import UserService from './UserService';
 export default function UserDashboard() {
     const [users, setUsers] = useState<any[]>([])
     const [userDetails, setUserDetails] = useState({id: "", name: "", email: ""});
-    const hostUrl = "http://localhost/restdemo/";
+    const hostUrl = "http://localhost/restdemo";
     const userEndpoint = "api/user/";
-    const userService =  UserService(hostUrl);
+    const userService = UserService(hostUrl);
     const userApiGetAll = hostUrl + userEndpoint;
     var loaded = false;
     useEffect(() => {
@@ -25,30 +25,37 @@ export default function UserDashboard() {
         <>
             <h1>USER LIST</h1>
             <table id="users">
+                <thead>
                 <tr>
                     <th>Name</th>
                 </tr>
+                </thead>
+                <tbody>
                 {users.map(user => {
                     return <tr>
                         <td><a onClick={() => getUserDetails(user.id)} href="#"> {user.name}</a></td>
                     </tr>
                 })}
-
+                </tbody>
             </table>
 
             <hr/>
             <h1>User Details</h1>
             <table id="users">
+                <thead>
                 <tr>
                     <th>ID</th>
                     <th>Name</th>
                     <th>Email</th>
                 </tr>
+                </thead>
+                <tbody>
                 <tr>
                     <td>{userDetails.id}</td>
                     <td>{userDetails.name}</td>
                     <td>{userDetails.email}</td>
                 </tr>
+                </tbody>
             </table>
         </>
 
