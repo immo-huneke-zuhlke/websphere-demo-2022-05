de4721f5d6f544022e0b64c1999f418c29b816c1
Immo Huneke <immo.huneke@zuhlke.com>
2022-05-16 10:10:39 +0100

    (fix): reduce compiler warnings

--- RestDemoTest/src/main/java/com/zuhlke/training/pact/restdemotest/StateMaker.java
+++ RestDemoTest/src/main/java/com/zuhlke/training/pact/restdemotest/StateMaker.java
@@ -1,35 +1,34 @@
 package com.zuhlke.training.pact.restdemotest;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
 
-interface SetupHolder{
+interface SetupHolder {
     void setup();
 }
+
 public class StateMaker {
 
-   static final Map<String, SetupHolder> pactStateMap = new HashMap() {{
-            SetupHolder setupHolder = StateMaker :: makeTwoUsers;
-            put("database contains two users", setupHolder);
+    static final Map<String, SetupHolder> pactStateMap = new HashMap<String, SetupHolder>() {{
+        put("database contains two users", StateMaker::makeTwoUsers);
     }};
 
     static void dispatch(String stateName) throws IllegalArgumentException, InvocationTargetException, IllegalAccessException {
-        if(pactStateMap.containsKey(stateName)){
+        if (pactStateMap.containsKey(stateName)) {
             pactStateMap.get(stateName).setup();
-        }else{
+        } else {
             throw new IllegalArgumentException("Illegal State : " + stateName);
         }
     }
 
-    static void makeTwoUsers(){
+    static void makeTwoUsers() {
         System.out.println("makeTwoUsers started");
         UserDao userDao = new UserDao();
         userDao.deleteAll();
-        userDao.saveUser(new User(1, "Gayatri","gpotawad@gmail.com"));
-        userDao.saveUser(new User(2, "Immo",null));
+        userDao.saveUser(new User(1, "Gayatri", "gpotawad@gmail.com"));
+        userDao.saveUser(new User(2, "Immo", null));
         System.out.println("makeTwoUsers done");
     }
 
