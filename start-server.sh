cce35c36d540591c737f1eb9d3a38a6bbdcd7398
Immo Huneke <immo.huneke@zuhlke.com>
2022-05-16 12:17:46 +0100

    (fix): Allow server to continue running after starting up

--- start-server.sh
+++ start-server.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env zsh
 
+cd "${0:A:h}"
+
 if [ -z "$1" ]
 then
   echo "You must specify servername"
   exit 1
 fi
-cp -v /Users/gapo/Project/GitHub/websphere-demo-2022-05/$1/target/*.war out/wlp/usr/servers/$1/apps
-docker exec -tt websphere_liberty /liberty/wlp/bin/server start $1
+cp -v ./$1/target/*.war out/wlp/usr/servers/$1/apps
+cmdline="/liberty/wlp/bin/server start $1 && tail -f /logs/messages.log"
+echo "exec -itt websphere_liberty bash -c \"$cmdline\""
+docker exec -itt websphere_liberty bash -c "$cmdline"
