0cd77e1fe275dfa4118b59e694ebfc0143aa5815
Immo Huneke <immo.huneke@handelsbanken.co.uk>
2022-05-18 22:30:50 +0100

    (feat): adapted scripts to Windows GitBash

--- start-pg.sh
+++ start-pg.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env zsh
 
-docker run -p 5432:5432 --name docker-postgres -e POSTGRES_PASSWORD=postgrespw -d postgres
+blocked=$(curl --noproxy '*' http://bbc.co.uk/ | grep -i 'Page Blocked' | wc -l)
+if [[ blocked -gt 0 ]]; then
+        image=artifactory.shbmain.shb.biz/docker-registry-1.docker.io/bitnami/postgresql:11.11.0-debian-10-r31
+else
+        image=postgres:latest
+fi
+
+cd "${0:A:h}"
+
+docker run -p 5432:5432 --name docker-postgres -e POSTGRES_PASSWORD=postgrespw -d $image
--- start-server.sh
+++ start-server.sh
@@ -8,6 +8,6 @@ then
   exit 1
 fi
 cp -v $1/target/*.war out/wlp/usr/servers/$1/apps
-cmdline="//liberty/wlp/bin/server start $1 && tail -f //logs/messages.log"
+cmdline="//liberty/wlp/bin/server start $1 && tail -f /logs/messages.log"
 echo "docker exec websphere_liberty bash -c \"$cmdline\""
 docker exec websphere_liberty bash -c "$cmdline"
